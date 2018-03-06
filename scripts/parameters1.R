7f7e2475857909ddf89a01524b13ea8fd45d27d1
Yelena <yelena.finegold@fao.org>
2018-03-06 06:35:28 +0300

    update parameters

--- scripts/parameters1.R
+++ scripts/parameters1.R
@@ -17,14 +17,11 @@ library(rgeos)
 #######          SET PARAMETERS FOR THE IMAGES OF INTEREST
 ####################################################################################
 ####### SET WHERE YOUR SCRIPTS ARE CLONED
-clonedir  <- paste0("~/safe_pc_tchad/")
+clonedir  <- paste0("~/eth_seg/")
 
 ####### DATA WILL BE CREATED AND STORED HERE
-rootdir    <- "~/eth_20180306/data"
-
-my_dir <- getwd()
-setwd(rootdir)
-rootdir <- getwd()
+setwd("~/eth_ws_20180306/data/")
+rootdir <- paste0(getwd(),"/")
 ####### SET WHERE YOUR IMAGE DIRECTORY IS
 rawimgdir   <- paste0(rootdir,'/',"raw_img")
 ####### SET WHERE YOUR TRAINING DIRECTORY IS
@@ -32,9 +29,9 @@ training_dir_local <- paste0(rootdir,'/', 'training')
 ## TRAINING DATA
 t1_train <- paste0(training_dir_local,'/',"training_points_7_4.csv")
 ## INPUT IMAGE FOR CLASSIFICATION
-t1_file  <- paste0(rootdir,'/',"lsat_s1_alos_srtm.vrt")
+t1_file  <- paste0(rootdir,'/',"landsat.vrt")
+# t1_file  <- paste0(rootdir,'/',"lsat_s1_alos_srtm.vrt")
 
-# seg_shp_tmp <- paste0(rootdir,'/',"tiles/rasterseg",'/seg_landsat_7_4.shp')
 
 ####### other directories
 oft.script.dir <- '~/scripts_ofgt/'
