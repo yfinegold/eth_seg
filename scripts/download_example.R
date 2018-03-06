33ee9bbd9187f7ea4089fde56bb81b12cd6a725e
Yelena <yelena.finegold@fao.org>
2018-03-06 06:24:43 +0300

    change example data

--- scripts/download_example.R
+++ scripts/download_example.R
@@ -4,8 +4,10 @@ wkdir <- "~/eth_ws_20180306"
 if(!dir.exists(wkdir)){dir.create(wkdir, recursive = T)}
 setwd(wkdir)
 
-system("wget https://www.dropbox.com/s/6twsqbbar0auoza/data.zip?dl=0")
-system("mv data.zip?dl=0 data.zip" )
+# system("wget https://www.dropbox.com/s/6twsqbbar0auoza/data.zip?dl=0")
+# system("mv data.zip?dl=0 data.zip" )
+system("wget https://www.dropbox.com/s/2o257azwqc14pay/data2.zip?dl=0")
+system("mv data2.zip?dl=0 data.zip" )
 
 system("unzip data.zip" )
 # system("chmod 755  ~/gnq_ws_20170726/scripts/oft-zonal_large_list.py")
