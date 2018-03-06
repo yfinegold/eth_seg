3d65ac28b94e29c15505a7af0555a874d5c8c4f7
Yelena <yelena.finegold@fao.org>
2018-03-06 06:50:57 +0300

    update example

--- scripts/download_example.R
+++ scripts/download_example.R
@@ -6,8 +6,8 @@ setwd(wkdir)
 
 # system("wget https://www.dropbox.com/s/6twsqbbar0auoza/data.zip?dl=0")
 # system("mv data.zip?dl=0 data.zip" )
-system("wget https://www.dropbox.com/s/2o257azwqc14pay/data2.zip?dl=0")
-system("mv data2.zip?dl=0 data.zip" )
+system("wget https://www.dropbox.com/s/daw0ya4ku4tcljz/data1.zip?dl=0")
+system("mv data1.zip?dl=0 data.zip" )
 
 system("unzip data.zip" )
 # system("chmod 755  ~/gnq_ws_20170726/scripts/oft-zonal_large_list.py")
